23b5decc1d5ee8bdb4580ea7bf48c3ce74e9ffc0
Abhinav Singh Verma <abhi1349@gmail.com>
2018-05-24 00:55:59 +0530

    Test-1

--- app/src/main/java/aadoo/system/com/caloriesappretrofit/activities/MainActivity.java
+++ app/src/main/java/aadoo/system/com/caloriesappretrofit/activities/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import aadoo.system.com.caloriesappretrofit.R;
 
@@ -20,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this,ListActivity.class);
         intent.putExtra("type","fruits");
         startActivity(intent);
+        Toast.makeText(this, "hi", Toast.LENGTH_SHORT).show();
     }
 
     public void getVegitables(View view) {
